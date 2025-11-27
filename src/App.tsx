8a058f50ad99d2382c614b63e2f888c8741b9481
williamuteich <williamuteich14@gmail.com>
2025-11-27 15:29:07 -0300

    ajustando basename

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/tw-servicos-digitais">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
